72eb7071fe0eb940323cf6ba1e32e06aa2ebf406
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-11-28 21:08:36 -0300

    19 section 2 layaut and css

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
-import { MainLayout } from "../components/layouts/MainLayout";
+import { MainLayout } from "../../components/layouts/MainLayout";
 
-export default function AboutPage() {
+export default function ContactPage() {
 	return (
 		<MainLayout>
-			<h1>AboutPage</h1>
+			<h1>ContactPage</h1>
 			<h1 className='title'>
 				Ir a <Link href="/">Home</Link>
 			</h1>
 
 			<p className='description'>
 				Get started by editing{" "}
-				<code className='code'>pages/about.tsx</code>
+				<code className='code'>pages/contact.tsx</code>
 			</p>
 		</MainLayout>
 	);
